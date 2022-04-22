90e9e11404fcae7c0e3c33a3d06c1fb997920cdd
Diana Balan <dyabalan98@gmail.com>
2022-04-22 22:33:06 +0300

    Initial commit

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,233 @@
 
-class ModelName:
+class LoginModel:
 
-    def vertex_A(self):
+    def fake_vertex(self):
         pass
 
-    def vertex_B(self):
+    def v_dashboard_page(self):
         pass
 
-    def edge_A(self):
+    def v_login_page(self):
+        pass
+
+    def e_load_login_page(self):
+        pass
+
+    def e_login_disabled_user(self):
+        pass
+
+    def e_login_empty_password(self):
+        pass
+
+    def e_login_empty_username(self):
+        pass
+
+    def e_login_enabled_standard_user(self):
+        pass
+
+    def e_login_invalid_credentials(self):
+        pass
+
+    def e_login_problem_user(self):
+        pass
+
+    def e_logout(self):
+        pass
+
+
+
+class AddProjectModelSimplified:
+
+    def v_add_customer_popup_opened(self):
+        pass
+
+    def v_add_project_page(self):
+        pass
+
+    def v_dashboard_page(self):
+        pass
+
+    def v_project_added(self):
+        pass
+
+    def v_projects_page(self):
+        pass
+
+    def e_add_customer_with_duplicate_name(self):
+        pass
+
+    def e_add_project_all_fields_valid_1_admin(self):
+        pass
+
+    def e_add_project_all_fields_valid_4_admins(self):
+        pass
+
+    def e_add_project_required_fields_only(self):
+        pass
+
+    def e_add_project_with_duplicate_admins(self):
+        pass
+
+    def e_add_project_with_duplicate_project_name(self):
+        pass
+
+    def e_add_project_with_empty_customer_name(self):
+        pass
+
+    def e_add_project_with_empty_project_name(self):
+        pass
+
+    def e_add_project_with_newly_added_customer(self):
+        pass
+
+    def e_add_project_with_nonexistent_admin(self):
+        pass
+
+    def e_add_project_with_nonexistent_customer_name(self):
+        pass
+
+    def e_cancel(self):
+        pass
+
+    def e_cancel_popup(self):
+        pass
+
+    def e_go_to_add_project_page(self):
+        pass
+
+    def e_go_to_dashboard_page(self):
+        pass
+
+    def e_go_to_projects_page(self):
+        pass
+
+    def e_open_add_customer_popup(self):
+        pass
+
+    def e_return_to_projects_page(self):
+        pass
+
+
+
+class ReviewPerformanceModel:
+
+    def v11(self):
+        pass
+
+    def v_activated_review(self):
+        pass
+
+    def v_add_review_page(self):
+        pass
+
+    def v_approved_review(self):
+        pass
+
+    def v_complete_review_popup_opened(self):
+        pass
+
+    def v_dashboard(self):
+        pass
+
+    def v_edit_review_page(self):
+        pass
+
+    def v_evaluate_review_page(self):
+        pass
+
+    def v_form_step_2_enabled(self):
+        pass
+
+    def v_in_progress_review(self):
+        pass
+
+    def v_inactive_review(self):
+        pass
+
+    def v_manage_reviews_page(self):
+        pass
+
+    def e__return_to_dashboard(self):
+        pass
+
+    def e_activate_with_valid_data(self):
+        pass
+
+    def e_cancel_complete_review_popup(self):
+        pass
+
+    def e_complete_review_with_valid_data(self):
+        pass
+
+    def e_complete_with_empty_finalization_fields(self):
+        pass
+
+    def e_complete_with_invalid_completed_date(self):
+        pass
+
+    def e_complete_with_invalid_final_rating(self):
+        pass
+
+    def e_complete_with_invalid_rating(self):
+        pass
+
+    def e_confirm_review_completion(self):
+        pass
+
+    def e_evaluate_review(self):
+        pass
+
+    def e_fill_due_date_less_than_start_date(self):
+        pass
+
+    def e_fill_duplicate_review_data(self):
+        pass
+
+    def e_fill_empty_date_fields(self):
+        pass
+
+    def e_fill_empty_employee_name(self):
+        pass
+
+    def e_fill_empty_fields_date_fields(self):
+        pass
+
+    def e_fill_empty_supervisor(self):
+        pass
+
+    def e_fill_end_date_less_than_start_date(self):
+        pass
+
+    def e_fill_invalid_supervisor(self):
+        pass
+
+    def e_fill_nonexistent_employee_name(self):
+        pass
+
+    def e_fill_valid_employee_name(self):
+        pass
+
+    def e_go_back(self):
+        pass
+
+    def e_go_to_add_review_page(self):
+        pass
+
+    def e_go_to_edit_review_page(self):
+        pass
+
+    def e_go_to_manage_review_page(self):
+        pass
+
+    def e_return_to_manage_reviews_page(self):
+        pass
+
+    def e_save_edited_review(self):
+        pass
+
+    def e_save_with_invalid_rating(self):
+        pass
+
+    def e_save_with_valid_data(self):
         pass
 
